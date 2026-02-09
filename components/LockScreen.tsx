dbf78e0da9f15faeac218f12584698a59112190e
minhcuong04 <22130032@st.hcmuaf.edu.vn>
2026-02-09 17:03:30 +0700

    v

--- components/LockScreen.tsx
+++ components/LockScreen.tsx
@@ -56,7 +56,7 @@ const LockScreen: React.FC<LockScreenProps> = ({ onUnlock }) => {
                 <img
                   alt="Couple"
                   className="w-full h-full object-cover rounded"
-                  src="./public/img/(209).JPG"
+                  src="/img/(209).JPG"
                 />
                 <div className="absolute -top-3 left-1/2 -translate-x-1/2 w-24 h-8 bg-white/40 backdrop-blur-sm border border-white/40 shadow-sm" />
               </div>
